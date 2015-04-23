9459017c6957b12a9838bb66f2f80794bfce6772
Jolie McCullough <jmccullough@texastribune.org>
2015-04-23 15:13:06 -0500

    add filters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,13 +6,7 @@ $(document).ready(function(){
 
   var FJS = FilterJS(inmates, '#inmates', {
     template: '#inmate-template',
-    search: {ele: '#searchbox', fields: ['race']}, // With specific fields
-    criterias:[
-      {field: 'race', ele: '#race_criteria input:checkbox'},
-      {field: 'sex', ele: '#sex_criteria input:checkbox'},
-      {field: 'age', ele: '#age_filter', type: 'range'},
-      {field: 'time_served', ele: '#time_served_filter', type: 'range'}
-    ],
+    search: {},
     callbacks: {
       afterFilter: function(result){
         $('#total_inmates').text(result.length);
@@ -20,51 +14,40 @@ $(document).ready(function(){
     }
   });
 
-  FJS.addCriteria({field: 'year', ele: '#year_filter', type: 'range', all: 'all'});
-  FJS.addCriteria({field: 'rating', ele: '#rating_filter', type: 'range'});
-  FJS.addCriteria({field: 'runtime', ele: '#runtime_filter', type: 'range'});
-  FJS.addCriteria({field: 'genre', ele: '#genre_criteria input:checkbox'});
+  FJS.addCriteria({field: 'race', ele: '#race_criteria input:checkbox'});
+  FJS.addCriteria({field: 'age', ele: '#age_filter', type: 'range'});
+  FJS.addCriteria({field: 'timeserved', ele: '#timeserved_filter', type: 'range'});
+  FJS.addCriteria({field: 'sex', ele: '#sex_criteria input:checkbox'});
 
-  /*
-   * Add multiple criterial.
-    FJS.addCriteria([
-      {field: 'race', ele: '#race_criteria input:checkbox'},
-      {field: 'sex', ele: '#sex_criteria input:checkbox'},
-      {field: 'age', ele: '#age_filter', type: 'range'},
-      {field: 'time_served', ele: '#time_served_filter', type: 'range'}
-    ])
-  */
 
   window.FJS = FJS;
 });
 
 function initSliders(){
-  $('#rating_slider').slider({
-    min: 8,
-    max: 10,
-    values:[8, 10],
-    step: 0.1,
+  $('#age_slider').slider({
+    min: 18,
+    max: 100,
+    values:[18, 100],
+    step: 5,
     range:true,
     slide: function( event, ui ) {
-      $('#rating_range_label' ).html(ui.values[ 0 ] + ' - ' + ui.values[ 1 ]);
-      $('#rating_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
+      $('#age_range_label' ).html(ui.values[ 0 ] + ' - ' + ui.values[ 1 ]);
+      $('#age_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
     }
   });
 
-  $('#runtime_slider').slider({
-    min: 50,
-    max: 250,
-    values:[0, 250],
-    step: 10,
+  $('#timeserved_slider').slider({
+    min: 0,
+    max: 40,
+    values:[0, 40],
+    step: 5,
     range:true,
     slide: function( event, ui ) {
-      $('#runtime_range_label' ).html(ui.values[ 0 ] + ' mins. - ' + ui.values[ 1 ] + ' mins.');
-      $('#runtime_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
+      $('#timeserved_range_label' ).html(ui.values[ 0 ] + ' - ' + ui.values[ 1 ]);
+      $('#timeserved_filter').val(ui.values[0] + '-' + ui.values[1]).trigger('change');
     }
   });
 
-  $('#genre_criteria :checkbox').prop('checked', true);
-  $('#all_genre').on('click', function(){
-    $('#genre_criteria :checkbox').prop('checked', $(this).is(':checked'));
-  });
+  $('#race_criteria :checkbox').prop('checked', true);
+  $('#sex_criteria :checkbox').prop('checked', true);
 }

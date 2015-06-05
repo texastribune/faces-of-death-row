3e3015122c5e6567efb2f943ec41a2657b2c91e7
Jolie McCullough <jmccullough@texastribune.org>
2015-06-05 17:22:28 -0500

    more spacing for pagination, better lightboxes, dropdowns

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -128,28 +128,11 @@
 
   //when lightbox opens
   $inmatesContainer.find('.open-lightbox').click(function() {
-    //var inmate = this.id;
     var parent = $(this).parent().attr('id');
 
     $inmatesContainer.find('.open').removeClass('open');
-    //$inmatesContainer.find('.white_content').addClass('hidden');
     $('#' + parent).toggleClass('open');
     $('body').addClass('fixed');
-
-    // $('#light-' + inmate).toggleClass('hidden');
-    // $('#fade-' + inmate).toggleClass('hidden');
-
-
-    //set lightbox top/left
-    // if($windowHeight < $windowWidth) {
-    //   var lightboxHeight = $('#light-' + inmate).height();
-    //   var lightboxWidth = $('#light-' + inmate).width();
-    //   var top = ( $windowHeight - lightboxHeight )/2;
-    //   var left = ( $windowWidth - lightboxWidth)/2;
-    //   $('#interactive .white_content').css({'top': top, 'left': left});
-    // } else {
-    //   $('#interactive .white_content').css({'top': '5%', 'left': '5%', height: '90%', width: '90%'});
-    // }
   });
 
   //when click prev or next
@@ -163,6 +146,23 @@
     }
     if ($(this).hasClass('next') && !$(this).hasClass('inactive')) {
       nextInmate(inmate, prevNext[1]);
+    }
+  });
+
+  //when keydown arrows
+  $(document).keydown(function(e) {
+    if( $('.inmate.open').length ) {
+      var parentEls = $('.inmate.open');
+      var inmate = parentEls.find('.open-lightbox').attr('id');
+      var prevNext = addPreviousNext(inmate, parentEls);
+      var noPrev = parentEls.find('.prev').hasClass('inactive');
+      var noNext = parentEls.find('.next').hasClass('inactive');
+
+      if (e.keyCode === 37 && !noPrev) {
+        prevInmate(inmate, prevNext[0]);
+      } else if (e.keyCode === 39 && !noNext) {
+        nextInmate(inmate, prevNext[1]);
+      }
     }
   });
 
@@ -186,20 +186,12 @@
 
   //moves back
   function prevInmate (inmate, prevID) {
-    // $('#light-' + inmate).toggleClass('hidden');
-    // $('#fade-' + inmate).toggleClass('hidden');
-    // $('#light-' + prevID).toggleClass('hidden');
-    // $('#fade-' + prevID).toggleClass('hidden');
     $('#' + inmate).parent().removeClass('open');
     $('#' + prevID).parent().addClass('open');
   }
 
   //moves forward
   function nextInmate(inmate, nextID) {
-    // $('#light-' + inmate).toggleClass('hidden');
-    // $('#fade-' + inmate).toggleClass('hidden');
-    // $('#light-' + nextID).toggleClass('hidden');
-    // $('#fade-' + nextID).toggleClass('hidden');
     $('#' + inmate).parent().removeClass('open');
     $('#' + nextID).parent().addClass('open');
   }
@@ -207,36 +199,22 @@
   //close lightbox
   $('.black_overlay').click(function() {
     var inmate = $(this).parent().attr('id');
-    // $('.black_overlay').addClass('hidden');
-    // $('.white_content').addClass('hidden');
     $('body').removeClass('fixed');
     $('#' + inmate).toggleClass('open');
   });
 
   $('.close-lightbox').click(function() {
     var inmate = $(this).parent().parent().attr('id');
-    // $('.black_overlay').addClass('hidden');
-    // $('.white_content').addClass('hidden');
     $('body').removeClass('fixed');
     $('#' + inmate).toggleClass('open');
   });
 
-  //resets if you go between lightbox mode and dropdown mode
-  // $(window).resize(function() {
-  //   var resizedWidth = $(window).width();
-
-  //   //figure out where lightbox should be placed on screen!!
-  //   if(resizedWidth > 460) {
-  //     $inmatesContainer.find('.open-lightbox').click(function() {
-  //       var inmate = this.id;
-  //       var lightboxHeight = $('#light-' + inmate).outerHeight();
-  //       var lightboxWidth = $('#light-' + inmate).outerWidth();
-  //       var top = ( $windowHeight - lightboxHeight )/2;
-  //       var left = ( $windowWidth - lightboxWidth)/2;
-  //       $('#interactive .white_content').css({'top': top, 'left': left});
-  //     });
-  //   }
-  // });
+  $('.info-button').click(function() {
+    var inmate = $(this).parent().parent().attr('id');
+    if ($('#' + inmate).hasClass('open')) {
+      $('#' + inmate).removeClass('open');
+    }
+  });
 
 })();
 
